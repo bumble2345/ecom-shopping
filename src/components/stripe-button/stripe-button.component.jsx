c7c6e85d826b65837b38000758bd7b1b302ab8cc
bumble2345 <varshithdosapati2000@gmail.com>
2021-11-02 21:36:18 +0530

    Initial commit

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51JrOihSI01ZQ4EJboc23ZF6FNMQ12rwT7obbQfEJOj7guCc9hQIV01VDXCb3sGxR9sOVT9MwxyhHZjpswrDGOyj700vdKTyHoW';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
